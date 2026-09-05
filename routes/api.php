bad969ed7c4f1b412cc358c067b021d5389ff9a0
Yazid Syafrudin <233200299@almaata.ac.id>
2026-09-06 02:51:38 +0700

    fix: add missing project and achievement routes in api.php

--- routes/api.php
+++ routes/api.php
@@ -5,9 +5,16 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\GuestbookController;
 use App\Http\Controllers\ContactMessageController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\AchievementController;
 
 Route::get('/guestbook', [GuestbookController::class, 'index']);
 Route::post('/guestbook', [GuestbookController::class, 'store']);
 Route::post('/guestbook/{id}/like', [GuestbookController::class, 'like']);
 
 Route::post('/contact', [ContactMessageController::class, 'store']);
+
+Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects/{slug}', [ProjectController::class, 'show']);
+
+Route::get('/achievements', [AchievementController::class, 'index']);
