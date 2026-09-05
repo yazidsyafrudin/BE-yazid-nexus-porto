340f132893a4d07ca0f49868fe8e5db6f333b10d
Yazid Syafrudin <233200299@almaata.ac.id>
2026-09-05 07:46:11 +0700

    fix: ignore 127.0.0.1 default and prioritize PGHOST and DATABASE_URL

--- config/database.php
+++ config/database.php
@@ -86,12 +86,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL', env('DB_URL')),
-            'host' => env('DB_HOST', env('PGHOST', '127.0.0.1')),
-            'port' => env('DB_PORT', env('PGPORT', '5432')),
-            'database' => env('DB_DATABASE', env('PGDATABASE', 'laravel')),
-            'username' => env('DB_USERNAME', env('PGUSER', 'root')),
-            'password' => env('DB_PASSWORD', env('PGPASSWORD', '')),
+            'url' => env('DATABASE_URL') ?: env('DB_URL'),
+            'host' => env('PGHOST') ?: (env('DB_HOST') === '127.0.0.1' ? (env('PGHOST') ?: '127.0.0.1') : env('DB_HOST', '127.0.0.1')),
+            'port' => env('PGPORT', env('DB_PORT', '5432')),
+            'database' => env('PGDATABASE', env('DB_DATABASE', 'laravel')),
+            'username' => env('PGUSER', env('DB_USERNAME', 'root')),
+            'password' => env('PGPASSWORD', env('DB_PASSWORD', '')),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
